48e5874ccbc0398a139f4e2d00b18ef7a96ccf32
Luc-Frederic Langis <lucf.langis@gmail.com>
2020-05-23 18:24:38 +0200

    feat(player): add new animations

--- src/Player/Skin.gd
+++ src/Player/Skin.gd
@@ -6,13 +6,13 @@ onready var anim: AnimationPlayer = $AnimationPlayer
 
 
 func _ready() -> void:
-    anim.connect("animation_finished", self, "_on_AnimationPlayer_animation_finished")
+	anim.connect("animation_finished", self, "_on_AnimationPlayer_animation_finished")
 
 
 func _on_AnimationPlayer_animation_finished(anim_name: String) -> void:
-    emit_signal("animation_finished", anim_name)
+	emit_signal("animation_finished", anim_name)
 
 
 func play(anim_name: String) -> void:
-    assert(anim_name in anim.get_animation_list())
-    anim.play(anim_name)
+	assert(anim_name in anim.get_animation_list())
+	anim.play(anim_name)
