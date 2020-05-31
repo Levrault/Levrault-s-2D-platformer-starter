cc11c57d8e417da65bf8aaff33f8f33e3b7bf163
unknown <lucf.langis@gmail.com>
2020-05-31 11:00:07 +0200

    feat(debug) add slow motion debug tool

--- src/UI/debug/DebugDock.gd
+++ src/UI/debug/DebugDock.gd
@@ -3,7 +3,6 @@ extends MarginContainer
 Contains UI widgets that display debug info about the game world
 """
 
-func _gui_input(event: InputEvent) -> void:
+func _unhandled_input(event: InputEvent) -> void:
 	if event.is_action_pressed('toggle_debug_menu'):
 		visible = not visible
-		accept_event()
