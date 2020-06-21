11922ba5661ee18c009cbca22da1ff60924611b8
unknown <lucf.langis@gmail.com>
2020-06-21 21:24:00 +0200

    feat(level) add dynamic loading

--- src/Autoload/Level.gd
+++ src/Autoload/Level.gd
@@ -8,9 +8,30 @@ var rooms := {}
 func load(world: String) -> void:
 	if world == 'test':
 		rooms = {
-			"RoomTest1": preload("res://src/Room/RoomTest1.tscn"),
-			"RoomTest2": preload("res://src/Room/RoomTest2.tscn"),
-			"RoomTest3": preload("res://src/Room/RoomTest3.tscn")
+			"RoomTest1": load("res://src/Room/RoomTest1.tscn"),
+			"RoomTest2": load("res://src/Room/RoomTest2.tscn"),
+			"RoomTest3": load("res://src/Room/RoomTest3.tscn")
+		}
+		Events.emit_signal("level_preload_finished")
+		print_debug("preload is over")
+		return
+
+	if world == 'test2':
+		rooms = {
+			"RoomTest4": load("res://src/Room/RoomTest4.tscn"),
+		}
+		Events.emit_signal("level_preload_finished")
+		print_debug("preload is over")
+		return
+
+	if world == 'test3':
+		rooms = {
+			"RoomTest1": load("res://src/Room/RoomTest1.tscn"),
+			"RoomTest2": load("res://src/Room/RoomTest2.tscn"),
+			"RoomTest3": load("res://src/Room/RoomTest3.tscn"),
+			"RoomTest4": load("res://src/Room/RoomTest4.tscn"),
+			"RoomTest5": load("res://src/Room/RoomTest5.tscn"),
+			"RoomTest6": load("res://src/Room/RoomTest6.tscn"),
 		}
 		Events.emit_signal("level_preload_finished")
 		print_debug("preload is over")
