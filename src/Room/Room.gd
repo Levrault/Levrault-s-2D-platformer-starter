3efec7d6a3ba2d23d77d07183c4e2483262af5e0
unknown <lucf.langis@gmail.com>
2020-06-21 15:10:24 +0200

    feat(level) move debug command to room

--- src/Room/Room.gd
+++ src/Room/Room.gd
@@ -1,5 +1,6 @@
 extends Node2D
 
+onready var player: Player = find_node("Player")
 
 func _ready():
 	Events.emit_signal(
@@ -15,3 +16,39 @@ func _ready():
 			"player_room_entered",
 			$Gates.get_node(Room.gate_to_spawn).get_node("Spawn").global_position
 		)
+
+
+func _unhandled_input(event: InputEvent) -> void:
+	if event.is_action_pressed("debug_restart_level"):
+		get_tree().reload_current_scene()
+		return
+
+	if event.is_action_pressed("debug_spawn"):
+		player.state_machine.transition_to("Spawn")
+		return
+
+	if event.is_action_pressed("debug_die"):
+		player.state_machine.transition_to("Die")
+		return
+
+	if event.is_action_pressed("debug_slow_time"):
+		Engine.time_scale = .1
+		return
+
+	if event.is_action_pressed("debug_accelerate_time"):
+		Engine.time_scale = Engine.time_scale + .1
+		return
+
+	if event.is_action_pressed("debug_reset_time"):
+		Engine.time_scale = 1.0
+		return
+
+	if event.is_action_pressed("debug_free_camera"):
+		player.is_handling_input = not player.is_handling_input
+		if not player.is_handling_input:
+			var free_camera: Camera2D = load("res://src/Tools/FreeCamera.tscn").instance()
+			free_camera.position = player.position
+			add_child(free_camera)
+		else:
+			get_node("FreeCamera").queue_free()
+
